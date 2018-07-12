c5772403840f16c8c12006c81e5f3bd5ae92483d
Michael Liu <miliu@protonmail.com>
2018-07-12 14:55:10 -0700

    add camel_to_snake()

--- Python/utils.py
+++ Python/utils.py
@@ -108,3 +108,30 @@ def to_row(v: np.ndarray) -> np.ndarray:
     if np.squeeze(v).ndim != 1:
         raise ValueError("unable to reshape into vector due to multiple non-one dimensions")
     return v.reshape((1, -1))
+
+
+def camel_to_snake(camel):
+    """
+    Converts a camelCase string into snake_case.
+
+    Up to twice as fast as a pre-compiled regex.
+
+    Parameters
+    ----------
+    camel : str
+        camelCase string.
+
+    Returns
+    -------
+    snake : str
+        snake_case string.
+
+    """
+    uppers = set("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    return ''.join(
+        [camel[0]]  # avoid prepending an underscore when the first letter is capital
+        + ['_'+char
+           if char in uppers
+           else char
+           for char in camel[1:]]
+    ).lower()
