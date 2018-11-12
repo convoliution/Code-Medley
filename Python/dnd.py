80db28fa407e58b82ce2a57cc5d701d5cf063946
Michael Liu <miliu@protonmail.com>
2018-11-12 14:08:55 -0800

    rewrite Dice to be more Pythonic and robust

--- Python/dnd.py
+++ Python/dnd.py
@@ -4,17 +4,10 @@ class Dice:
     """
     Object and utility class for simulating dice rolls.
 
-    Invoking rolls is fairly ergonomic thanks to an abuse of Python's special functions.
-    Right multiplication to a Dice instance by an integer specifies the number of dice.
-    Left multiplication to a Dice instance by an integer specifies the number of faces.
-
-    More concretely, to roll four twenty-sided dice (commonly notated as "4d20"):
-    >>> d = Dice()
-    >>> 4*d*20
-    array([11, 15, 18,  7])
-
-    If the number of dice is not specified (e.g. `d*20`), it defaults to 1.
-    A `Dice` instance resets itself after each roll, and thus can be reused ad infinitum.
+    As an example, to roll three twenty-sided dice (commonly notated as "3d20"):
+    >>> from dnd import Dice as d
+    >>> 3*d(20)
+    (15, 18,  7)
 
     """
     @staticmethod
@@ -27,22 +20,22 @@ class Dice:
 
     @staticmethod
     def roll_hit_points(num_faces, con, level):
+        if not isinstance(num_faces, int) or num_faces < 1:
+            raise ValueError("number of faces must be a positive integer")
+        if not isinstance(con, int):
+            raise ValueError("constitution modifier must be an integer")
+        if not isinstance(level, int) or level < 1:
+            raise ValueError("level must be a positive integer")
         return num_faces + con \
              + np.random.randint(1, num_faces+1, level-1).sum() \
              + con*(level-1)
 
-    def __init__(self):
-        self.num_dice = 1
+    def __init__(self, num_faces):
+        if not isinstance(num_faces, int) or num_faces < 1:
+            raise ValueError("number of faces must be a positive integer")
+        self.num_faces = num_faces
 
     def __rmul__(self, num_dice):
-        if not isinstance(num_dice, int):
-            raise ValueError("number of dice must be an integer")
-        self.num_dice = num_dice
-        return self
-
-    def __mul__(self, num_faces):
-        if not isinstance(num_faces, int):
-            raise ValueError("number of faces must be an integer")
-        rolls = np.random.randint(1, num_faces+1, self.num_dice)
-        self.num_dice = 1
-        return tuple(rolls)
+        if not isinstance(num_dice, int) or num_dice < 1:
+            raise ValueError("number of dice must be a positive integer")
+        return tuple(np.random.randint(1, self.num_faces+1, num_dice))
